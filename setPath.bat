b452e7632aa9f0588e3f5db263ab66a6ab47f1ff
Zak Porche <zakporche45@gmail.com>
2024-01-08 18:17:28 -0700

    Initial commit

---
+++ setPath.bat
@@ -0,0 +1,3 @@
+@cd C:\Users\porchez\OneDrive - Maricopa County Sheriff's Office\restaurant
+@set PATH=C:\Users\porchez\OneDrive - Maricopa County Sheriff's Office\software\node-v20.10.0-win-x64\node-v20.10.0-win-x64;%PATH%
+@cmd.exe /K
